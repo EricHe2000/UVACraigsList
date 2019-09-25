4c35c19847d560f41d43198f1124712d19f70f06
jhc5wf <jhc5wf@virginia.edu>
2019-09-25 18:26:09 -0400

    admins can see models now

--- mysite/uvaCraigslist/admin.py
+++ mysite/uvaCraigslist/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category
+from .models import Topic
+from .models import Post
 
+admin.site.register(Category)
+admin.site.register(Topic)
+admin.site.register(Post)
 # Register your models here.
