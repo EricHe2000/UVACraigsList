54bb7532636bf1b2bdc9f2402dd285564f5cdbd1
jhc5wf <jhc5wf@virginia.edu>
2019-09-25 17:55:04 -0400

    bare minimum functioning. models added and Categories visible on 127.0.0.1

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#from django.conf.urls import url
+
+from uvaCraigslist import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
--- mysite/uvaCraigslist/views.py
+++ mysite/uvaCraigslist/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Category
 
+def home(request):
+    #return HttpResponse('If you can seeing this, Jake hasnt broken everything yet')
+    categories = Category.objects.all()
+    categories_names = list()
+
+    for category in categories:
+        categories_names.append(category.name)
+
+    response_html = '<br>'.join(categories_names)
+
+    return HttpResponse(response_html)
 # Create your views here.
