e3388fb345fab13c46ce226ae53774ceea93d0d1
Henry Nixon <hcn2ey@virginia.edu>
2019-10-13 14:53:17 -0400

    We have a user profile but need to fill in other data

--- login/models.py
+++ login/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete= models.DO_NOTHING)
+    #description = models.CharField(max_length=100, default='')
+    address = models.CharField(max_length=100, default='')
+    phone = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='profile_image', blank=True)
+    email = models.CharField(max_length=100, default='')
+   
+    def __str__(self):
+        return self.user.username
+
+    def create_profile(sender, **kwargs):
+	    if kwargs['created']:
+	        user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+#spost_save.connect(create_profile, sender=User)
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name="login/index.html")),
     path('accounts/', include('allauth.urls')),
+    path('accounts/profile/',include('login.urls')),
 ]
