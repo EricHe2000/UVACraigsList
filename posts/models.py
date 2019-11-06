dc4960f528b3a0fa8b4dfb834333b604bef19ce1
jhc5wf <jhc5wf@virginia.edu>
2019-11-06 16:35:27 -0500

    made categories drop down

--- posts/models.py
+++ posts/models.py
@@ -8,7 +8,18 @@ class Post(models.Model):
     titleText = models.CharField(max_length=200)
     description = models.CharField(max_length=200)
     creation_date = models.DateTimeField(auto_now=True)
-    category = models.CharField(max_length=200)
+    
+    Electronics = 'Electronics'
+    Services = 'Services'
+    Miscellaneous = 'Misc'
+    CategoryChoices = [
+        (Electronics, 'Electronics'),
+        (Services, 'Services'),
+        (Miscellaneous, 'Miscellaneous'),
+    ]
+    
+    category = models.CharField(max_length=200, choices=CategoryChoices, default=Miscellaneous)
+    
     price = models.DecimalField(max_digits=6, decimal_places=2)
     #https://djangopackages.org/packages/p/django-location-field/
     #user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
