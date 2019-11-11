86e549bcc637f2cd8335014ddf725514eaff6d9f
A-Ferraro <cbf6yd@virginia.edu>
2019-11-11 04:10:37 -0500

    Gave the site a makeover and made the category system work.

--- login/views.py
+++ login/views.py
@@ -4,6 +4,9 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from login.forms import EditProfileForm
+from posts.models import Post
+
+
 
 # def profile(request, pk=None):
 #     if pk:
@@ -30,16 +33,20 @@ def view_profile(request, pk=None):
         user = User.objects.get(pk=pk)
     else:
         user = request.user
+      
+    
     args = {'user': user}
     return render(request, 'login/profile.html', args)
 
 def index(request):
-	context = {
-		'user': User.objects.order_by('-date')
-	if request.user.is_authenticated else []
-	}
 
-	return render(request, 'login/profile.html', context)
+    context = {
+
+        'user': User.objects.order_by('-date')
+    if request.user.is_authenticated else []
+    }
+
+    return render(request, 'login/profile.html', context)
 
 
 def logout_view(request):
