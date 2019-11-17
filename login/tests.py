d74cbb739c04e20071b007600b725f3850584736
Eric He <eriche2000@gmail.com>
2019-11-16 21:04:11 -0500

    added test case

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.contrib.auth.forms import UserCreationForm
+from .forms import *
 
 # Create your tests here.
+def test_PrrofileUpdateForm_invalid(self):
+        form = EditProfileForm(data={ 'email':'eriche2000@gmail.com ',
+                                        'first_name':'Eric',
+                                        'last_name':'He',
+                                        })
+        self.assertFalse(form.is_valid())
