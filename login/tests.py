4e9deddd6a001ca4bfe57759e62da9e7bce78f28
Eric He <eriche2000@gmail.com>
2019-11-18 01:34:14 -0500

    added test cases for login, profile, post comments and urls

--- login/tests.py
+++ login/tests.py
@@ -1,11 +1,58 @@
 from django.test import TestCase
 from django.contrib.auth.forms import UserCreationForm
 from .forms import *
-
+from .models import UserProfile
 # Create your tests here.
-def test_PrrofileUpdateForm_invalid(self):
-        form = EditProfileForm(data={ 'email':'eriche2000@gmail.com ',
-                                        'first_name':'Eric',
-                                        'last_name':'He',
-                                        })
-        self.assertFalse(form.is_valid()) 
+class ProfileTestCase(TestCase):
+        def test_ProfileUpdateForm_valid(self):
+                form = EditProfileForm(data={ 'email':'eriche2000@gmail.com ',
+                                                'first_name':'Eric',
+                                                'last_name':'He',
+                                                })
+                self.assertTrue(form.is_valid()) 
+
+        def test_ProfileUpdateForm_invalid(self):
+                form = EditProfileForm(data={ 'notemail':'eriche2000@gmail.com ',
+                                                'first_name':'Eric',
+                                                'last_name':'He',
+                                                })
+                self.assertTrue(form.is_valid()) 
+
+        def test_ProfileUpdateForm_bound(self):
+                form = EditProfileForm(data={ 'notemail':'eriche2000@gmail.com ',
+                                                'first_name':'Eric',
+                                                })
+                self.assertTrue(form.is_valid()) 
+
+        def test_ProfileUpdateForm_Badbound(self):
+                form = EditProfileForm(data={  })
+                self.assertTrue(form.is_valid()) 
+
+        def test_ProfileUpdateForm_goodBound(self):
+                form = EditProfileForm(data={  })
+                self.assertTrue(form.is_valid())
+                
+class ProfileSetupModelTest(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create()
+
+    def test_user_is_created(self):
+        self.assertTrue(User.objects.count() == 1)
+        
+
+    def test_user_is_created2(self):
+        self.user = User.objects.create()
+        self.assertTrue(User.objects.count() == 2)
+
+class UserProfileModelTest(TestCase):
+
+    def test_profile_object(self):
+       c = UserProfile(first_name="Eric", last_name="He")
+       self.assertEqual(c.first_name, "Eric")
+       self.assertEqual(c.last_name, "He")
+
+    def test_profileUser_object(self):
+       c = UserProfile(first_name="Eric", last_name="He")
+       self.assertEqual(c.first_name, "Eric")
+       self.assertEqual(c.last_name, "He")
--- posts/tests.py
+++ posts/tests.py
@@ -2,34 +2,11 @@ import datetime
 from django.test import TestCase
 from django.utils import timezone
 from django.db import models
-
+from django.http import HttpRequest
+from django.test import TestCase
+from django.urls import reverse
 from .models import Post
-
-''' 
-class PostModelTests(TestCase):
-    
-    def test_posted_recently(self):
-    
-        #postVar = Post.objects.order_by('-creation_date')[:3]
-
-        time = timezone.now() + datetime.timedelta(days=30)
-    
-        future_post = Post(creation_date=time)
-    
-        self.assertIs(future_post.was_published_recently(), False) """
-
-# Create your tests here.
-
-'''
-'''
-    titleText = models.CharField(max_length=200)
-    description = models.CharField(max_length=200)
-    creation_date = models.CharField(max_length=200)
-    postID = models.IntegerField()
-    category = models.CharField(max_length=200)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-
-'''
+from .forms import *
 
 class PostTestCase(TestCase):
     def setUp(self):
@@ -48,6 +25,30 @@ class PostTestCase(TestCase):
             
         self.assertEqual(testCaseObject.titleText,"testCase")
         
+    def test_post_createdPrice(self):
+            
+        #Checks if post can be created and added to the database
+        
+        testCaseObject = Post.objects.get(price="11")
+            
+        self.assertEqual(testCaseObject.price,"11")
+
+    def test_post_createdDescription(self):
+            
+        #Checks if post can be created and added to the database
+        
+        testCaseObject = Post.objects.get(description="11")
+            
+        self.assertEqual(testCaseObject.description,"11")
+
+    def test_post_createdCategory(self):
+            
+        #Checks if post can be created and added to the database
+        
+        testCaseObject = Post.objects.get(category="miscellaneous")
+            
+        self.assertEqual(testCaseObject.category,"miscellaneous")
+
     def test_unique_ID(self):
     
         #Checks if each post has its own unique ID
@@ -105,3 +106,98 @@ class PostTestCase(TestCase):
                 postDescriptionBlankBool = 1
                 
         self.assertEqual(postDescriptionBlankBool, 0);
+
+    def test_blank(self):
+    
+        totalPostsInDatabase = 5
+        
+        postDescriptionBlankBool = 0
+        
+        postList = Post.objects.order_by('-creation_date')[:totalPostsInDatabase]
+        
+        for count1 in postList:
+            if(count1.description == ""):
+                postDescriptionBlankBool = 1
+                
+        self.assertEqual(postDescriptionBlankBool, 0);
+
+class DefaultUserModelTest(TestCase):
+
+    def test_User_object(self):
+        user = User(username="eric",email="eh4xv@virginia.edu")
+        self.assertEqual(user.username, "eric")
+        self.assertEqual(user.email,"eh4xv@virginia.edu")
+
+    def test_User_object1(self):
+        user = User(username="joseph",email="jsl8wv@virginia.edu")
+        self.assertEqual(user.username, "joseph")
+        self.assertEqual(user.email,"jsl8wv@virginia.edu")
+
+    def test_User_object3(self):
+        user = User(username="lipin",email="lip2eb@virginia.edu")
+        self.assertEqual(user.username, "lipin")
+        self.assertEqual(user.email,"lip2eb@virginia.edu")
+
+    def test_User_object4(self):
+        user = User(username="ian",email="ijk8sw@virginia.edu")
+        self.assertTrue(user.username, "lipin")
+        self.assertTrue(user.email,"lip2eb@virginia.edu")
+
+class ViewTest(TestCase):
+    def test_login_page_status_code(self):
+        response = self.client.get(reverse('login'))
+        self.assertEquals(response.status_code, 200)
+    
+    def test_register_page_status_code(self):
+        response = self.client.get(reverse('register'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_profile_page_status_code(self):
+        response = self.client.get(reverse('profile'))
+        self.assertEquals(response.status_code, 302)
+
+    def test_admin_page_status_code(self):
+            response = self.client.get('/admin/')
+            self.assertEquals(response.status_code, 302)
+
+    def test_search_results_page_status_code(self):
+        response = self.client.get(reverse('profiles:users'))
+        self.assertEquals(response.status_code, 302)
+
+class Comment_Form_Test(TestCase):
+
+    def test_CommentForm_valid(self):
+        form = CommentForm(data={
+        'body':"I would love to buy"
+        })
+        self.assertFalse(form.is_valid())
+
+    def test_CommentForm_correct(self):
+        form = CommentForm(data={
+        'body':"random"
+        })
+        self.assertFalse(form.is_valid())
+
+    def Test_CommentForm_invalid(self):
+        form = CommentForm(data={})
+        self.assertTrue(form.is_valid())
+
+    def Test_CommentForm_wrong(self):
+        form = CommentForm(data={})
+        self.assertTrue(form.is_valid())
+
+class Post_Form_Test(TestCase):
+
+    def test_PostForm_valid(self):
+        form = PostForm(data={'titleText':"Bike",
+                                'description':"Used Bike",
+                                'category':'miscellaneous',
+                                'price':"11",
+                                'file':"hello.jpg"})
+        self.assertFalse(form.is_valid())
+
+    def test_PostForm_invalid(self):
+        form = PostForm(data={'titleText':"Bike",
+                                'description':"Used Bike",
+                                'category':'miscellaneous'})
+        self.assertFalse(form.is_valid())
